b83154b8d9c9f73d9d266a960859035842389a5d
OfirShitrit <ofirha18@gmail.com>
2023-01-01 16:32:41 +0200

    change getNumOfLines functions

--- src/Ex2_1.java
+++ src/Ex2_1.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,7 +13,7 @@ public class Ex2_1 {
                 String nameFile = "file_" + (i+1);
                 namesOfFiles[i] = nameFile;
                 FileWriter myWriter = new FileWriter(nameFile);
-                Random rand = new Random(seed);
+                Random rand = new Random(seed); // TODO - make help funtion
                 int numOfRows = rand.nextInt(bound);
                 for (int j = 1; j <= numOfRows ; j++) {
                     myWriter.write("Hello World!\n"); // TODO - what to write?
@@ -28,16 +27,21 @@ public class Ex2_1 {
         return namesOfFiles;
     }
 
-    private static int numLines(String fileName) throws FileNotFoundException {
-        Path path = Paths.get(fileName);
+
+
+    private static int numLines(String path) throws FileNotFoundException {
+        File file = new File(path);
         int lines = 0;
-        try {
-            lines = (int) Files.lines(path).count();
-        }catch (IOException e) {
-            e.printStackTrace();
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine()){
+            sc.nextLine();
+            lines++;
         }
         return lines;
     }
+
+
     public static int getNumOfLines(String[] fileNames) throws IOException {
         int totalNumOfRows = 0;
 
@@ -46,4 +50,16 @@ public class Ex2_1 {
         }
         return totalNumOfRows;
     }
+
+    public int getNumOfLinesThreads(String[] fileNames){
+        int totalNumOfRows = 0;
+        for(String fileName : fileNames){
+            NumOfLinesThread numOfLinesThread = new NumOfLinesThread(fileName);
+        }
+
+        return totalNumOfRows;
+
+    }
 }
+
+
---
+++ src/NumOfLinesThread.java
@@ -0,0 +1,13 @@
+
+public class NumOfLinesThread extends Thread {
+    String fileName;
+
+    public NumOfLinesThread(String fileName){
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
